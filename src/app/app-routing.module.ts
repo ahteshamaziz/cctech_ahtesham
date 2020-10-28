711b4f44187e8d5352311bbbcbe1d74949969e8b
Mohammad Ahtesham <md@brsindia.com>
2020-10-28 23:40:48 +0530

    second commite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path:'home',component: HomeComponent,children:  [
+  //  {path: 'home/:username/:c', component: HomeComponent},
+  ]},
+  // {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor( private router: Router) { }
+
+   name: any;
+   password:any;
+
+  ngOnInit(): void {
+  }
+
+  submit(){
+    alert(this.name);
+    alert(this.password);
+    this.router.navigate(['/home']);
+
+  }
+
+}
